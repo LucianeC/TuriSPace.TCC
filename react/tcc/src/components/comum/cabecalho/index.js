3e8434752830c7013d97d02c47860dc84e17161f
LucianeC <costaluciane087@gmail.com>
2021-10-04 15:27:04 -0300

    uhu

---
+++ react/tcc/src/components/comum/cabecalho/index.js
@@ -0,0 +1,11 @@
+export default function cabecalho () {
+    return (
+        <container-cabecalho>
+            <div className="cabecalho">
+                <div className="logo"> </div>
+                div.
+            </div>
+
+        </container-cabecalho>
+    )
+}
