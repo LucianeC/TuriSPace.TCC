518686c9f5313559d39391fd552c567afdc73211
LuianeC <costaluciane087@gmail.com>
2021-10-04 22:40:40 -0300

    lucy

--- react/tcc/src/components/comum/cabecalho/index.js
+++ react/tcc/src/components/comum/cabecalho/index.js
@@ -1,11 +1,8 @@
-export default function cabecalho () {
-    return (
-        <container-cabecalho>
-            <div className="cabecalho">
-                <div className="logo"> </div>
-                div.
-            </div>
-
-        </container-cabecalho>
-    )
-}
+<container-cabecalho>
+            <img src="/assets/imagens/logo1.svg" alt="" />
+            <div onClick> Inicio </div>
+            <div onClick> Lugares </div>
+            <div onClick> Fale Conosco </div>
+            <div onClick> Meus Pacotes </div>
+            <div className= "minhaConta"> Minha <br>conta</br> <img src="/assets/imagens/perfil.svg" alt="" /> </div>
+</container-cabecalho>
